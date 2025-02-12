68bb514164cb4e274f5342e042b56ab10581ee1b
isabellaTroncoso <isabella.troncoso@stud.sti.se>
2025-02-12 16:22:34 +0100

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Student } from './types/Student';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [student, setStudent] = useState<Student>({
+    name: "Liam",
+    age: 20,
+    grade: "B",
+  });
+
+  // Funktion för att uppdatera studentdata
+  const updateStudent = () => {
+    setStudent({
+      name: "Olivia",
+      age: 18,
+      grade: "A",
+    });
+  };
 
   return (
     <>
+     <h1>Studentprofil</h1>
+      <p>
+        Student: {student.name}, Age: {student.age}, Grade: {student.grade}
+      </p>
+      <button onClick={updateStudent}>Uppdatera student</button>
       <div>
       <p>{2 < 4 ? "True" : "False"}</p>
 
@@ -21,4 +41,5 @@ function App() {
   )
 }
 
-export default App
+export default App;
+
